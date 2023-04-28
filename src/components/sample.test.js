8828b3a5adf8300459f85970bd59cee7a63771ae
Andrew McKendrick <ditus@rocketmail.com>
2023-04-28 11:29:28 -0400

    Removes the sample test

--- src/components/sample.test.js
+++
@@ -1,9 +0,0 @@
-//
-// Copyright (c) DITUS INC. All rights reserved. See LICENSE file in the project
-// root for details.
-//
-describe('Sample', () => {
-  it('delete at a later date.', () => {
-    expect(1).toBe(1);
-  });
-});
